c97129a1cd2ece1cc479b070bfbe47b37394c049
Alex Truong <alex@MBP-15.local>
2020-10-22 15:20:05 -0700

    v2 completion, added semi-colons

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -8,11 +8,11 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year) {
     	if (year % 400 == 0) {
-    		return True
+    		return True;
     	} else if (year % 4 == 0 && year % 100 != 0) {
-    		return True
+    		return True;
     	} else {
-    		return False
+    		return False;
     	}
     }
     private static void checkLeapYear(int year) {

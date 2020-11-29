a1c95e3f44e3f9b09a3e2bbe79e4db25c940385a
Alex Truong <altruong@berkeley.edu>
2020-11-28 18:55:27 -0800

    implemented dcatenate + catenate methods

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,20 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList pointerA = A;
+        if (A == null && B != null) {
+            return B;
+        } else if (B == null && A!= null) {
+            return A;
+        }
+        while (pointerA != null) {
+            if (pointerA.rest == null) {
+                pointerA.rest = B;
+                break;
+            }
+            pointerA = pointerA.rest;
+        }
+        return A;
     }
 
     /**
@@ -90,8 +102,10 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null && B != null) {
+            return B;
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 

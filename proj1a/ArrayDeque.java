6e127386fbd4846ba0081234d6fff114714fe549
Alex Truong <altruong@berkeley.edu>
2020-12-19 17:02:20 -0800

    rewrote adding, ordering, and resizing operations

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,5 +1,5 @@
 public class ArrayDeque<T> {
-    public T[] items;
+    private T[] items;
     private int size;
     private int nextFirst;
     private int nextLast;
@@ -9,16 +9,43 @@ public class ArrayDeque<T> {
  *   nextLast - 1 = last item in the deque
  *   size is always the amount of items in the list.
  */
-    /* ArrayDeque Constructor */
+    /* Creates an empty list  */
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
-        nextFirst = 0;
-        nextLast = 1;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    private void reorganize() {
+        T[] tmp_arr = (T[]) new Object[items.length];
+        int num_elements_first = 0;
+        int num_elements_back = 0;
+        int last_index = 0;
+        if (nextFirst+1 > 4) { // Copies first elements at the END of the underlying array (1st first elems)
+            num_elements_first = ((items.length-1)-nextFirst);
+            System.arraycopy(items, nextFirst+1, tmp_arr, 0, num_elements_first);
+            nextFirst = 0;
+        }
+        if (nextFirst < 4) { // Copies first elements at the START of the underlying array (2nd first elems)
+            int temp = (5-(nextFirst));
+            System.arraycopy(items, nextFirst, tmp_arr, num_elements_first, temp);
+            last_index = temp + num_elements_first;
+        }
+        if (nextLast >=  5) { // Copies any elements at START of underlying array (1st last elems)
+            num_elements_back = (nextLast-5);
+            System.arraycopy(items, 5, tmp_arr, last_index, num_elements_back);
+        }
+        if (nextLast < 5) { // Copies any elements at END of underlying array (2nd last elems)
+            int temp = (5-nextLast);
+            int start_index = num_elements_first + last_index + num_elements_back;
+            System.arraycopy(items, nextLast, tmp_arr, start_index, temp);
+        }
+        items = tmp_arr;
     }
 
     private double capacity() {
-        double usage_factor = (double)size/items.length; // Need  to cast the ints into doubles, otherwise may get 0.
+        double usage_factor = (double) size / items.length; // Need  to cast the ints into doubles, otherwise may get 0.
         if (items.length < 16 && (usage_factor < 0.25)) {
             return items.length; // Do nothing if array size is <16 and
         } else if (usage_factor > 0.25) {
@@ -26,136 +53,79 @@ public class ArrayDeque<T> {
         } else {
             return 0.5;
         }
-/*      1. Performing a check on item deletion in array -> resize if extra space is unnecessary
-            1a. If there are items at the end of the array (size 3, but array.length = 1000)
-            1a cont. THEN -> Then resize and move those items into the new array (condensing)
-        2. Array.length < 16 -> usage factor can be arbitrarily low
-        3. Array.length >= 16 -> usage factor = 0.25; */
     }
-    /* Resizes arrays  */
+
     private void resize() {
+        // Before resizing operation, need to sort the array.
+        reorganize();
         double newCapacity = items.length * capacity();
         T[] tmp_arr = (T[]) new Object[(int)newCapacity];
         System.arraycopy(items, 0, tmp_arr, 0, size);
         items = tmp_arr;
     }
 
-    /* Updates nextFirst and nextLast if they reach their ends (Circular array implementation) */
-    private void updateNext() {
-        double usage_factor = (double)size/items.length;
-        if (size == items.length || (usage_factor > 0.25 && items.length >= 16)) {
-            nextLast = size; // Re-adjust the nextLast to be the indice of the first null value in new array;
-            resize();
-            nextFirst = items.length-1; // Re-adjust nextFirst to the new array's very last indice
-        } update_next_first();
-          update_next_last();
-    }
+    /* Updates nextFirst */
     private void update_next_first() {
         if (nextFirst == -1) {
-            nextFirst = items.length - 1; // Sets nextFirst to end of the list
-        } else if (nextFirst == items.length+1) {
-            nextFirst = 0; // If you're at the end of the list and deleting, reset back to 0.
+            nextFirst = items.length - 1; // Continues at end of underlying array.
         }
     }
     private void update_next_last() {
-        if (nextLast == items.length) {
+        if (nextLast > items.length - 1) {
             nextLast = 0;
         }
+    }
+    public void update_next_both() {
+        if (size == items.length) {
+            int nextLast_start_pos = items.length;
+            resize();
+            nextLast = nextLast_start_pos;
+            nextFirst = items.length - 1;
+        } update_next_first();
+        update_next_last();
     }
 
     /* Adds an item of type T to the front of the deque */
     public void addFirst(T item) {
-        updateNext();
+        size += 1;
         items[nextFirst] = item;
-        nextFirst -= 1; // Null case because -> nextFirst = 0 -> -1 -> code breaks;
-        size += 1;         // Need to move and resize array if no space.
+        nextFirst -= 1;
+        update_next_both();
     }
 
-    /* Adds an item of type T to the back of the deque */
     public void addLast(T item) {
-        updateNext();
+        size += 1;
         items[nextLast] = item;
         nextLast += 1;
-        size += 1;
-    }
-
-    /** Removes and returns the item at the front of the deque
-     * If no such item exists, returns null.
-     * [$] Implemented anti-loitering condition
-     *                                                      */
-    public T removeFirst() {
-        double usage_factor = (double)size/items.length;
-        if (items[nextFirst+1] == null) {
-            return null;
-        } else if (usage_factor < 0.25) {
-            resize();
-        } size -= 1;
-        T first_item = items[nextFirst+1];
-        items[nextFirst + 1] = null;
-        nextFirst = nextFirst+1;
-        updateNext();
-        return first_item;
-    }
-
-    /** Removes and returns the item at the back of the deque
-     * If no such item exists, returns null.
-     * [$] Implemented anti-loitering condition
-     *                                                      */
-    public T removeLast() {
-        double usage_factor = (double)size/items.length;
-        if (items[nextLast-1] == null) {
-            return null;
-        } else if (usage_factor < 0.25) {
-            resize();
-        } size -= 1;
-        T last_item = items[nextLast-1];
-        items[nextLast - 1] = null;
-        nextLast = nextLast - 1;
-        updateNext();
-        return last_item;
-    }
-
-    /* Gets the item at the given index */
-    public T get (int index) {
-        if (items[index] == null) {
-            return null;
-        } return items[index];
-    }
-
-    /* Returns the number of items in the deque
-    [*] Implemented in constant time using caching */
-    public int size() {
-        return size;
+        update_next_both();
     }
 
     /* Returns true if deque is empty, false otherwise */
     public boolean isEmpty() {
         if (size == 0) {
             return true;
-        } return false;
-    }
-
-    /* Prints the items in the deque from first to last, separated by a space */
-    // [!!!] NEED TO FIND MORE TIME EFFICIENT WAY TO PRINT FOR LARGER ARRAYS
-    public void printDeque() {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null ) {
-                System.out.print(items[i] + " ");
-            }
         }
+        return false;
     }
 
-    /*
+    /* Gets the item at the given index */
+    public T get(int index) {
+        if (items[index] == null) {
+            return null;
+        } return items[index];
+    }
+
     public static void main(String[] args) {
         ArrayDeque<Integer> test = new ArrayDeque<>();
-        for (int i = 0; i < 1000; i++) {
+        test.addFirst(-1);
+        test.addLast(1000);
+        for (int i = 0; i < 100; i++) {
+            test.addFirst(i);
+        }
+        for (int i = 200; i < 300; i++) {
             test.addLast(i);
         }
-        for (int i = 0; i < 900; i++) {
-            test.removeLast();
-        }
-        test.addFirst(10);
-        test.addFirst(20);
-        test.printDeque();
-    } */
+        test.isEmpty();
+    }
+
 }
